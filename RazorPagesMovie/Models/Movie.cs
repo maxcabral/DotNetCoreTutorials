54df301bb84cff3d2a990ec933264c89240bcd89
Max Cabral <max@maxcabral.com>
2018-08-19 22:36:39 -0700

    Add progress up to LocalDB
    https://docs.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/sql?view=aspnetcore-2.1

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
